0fc25f4c555ab95c1027ec8d364e139d7f95137f
Hidalgo <hidalgochpnce@gmail.com>
2017-10-11 21:31:50 -0500

    Se actualizo numeros telefonicos

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Artesaos\SEOTools\Providers\SEOToolsServiceProvider::class,
+//        Artesaos\SEOTools\Providers\SEOToolsServiceProvider::class,
     ],
 
     /*
@@ -227,11 +227,11 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
         // other Facades ommited
-        'SEOMeta'   => Artesaos\SEOTools\Facades\SEOMeta::class,
-        'OpenGraph' => Artesaos\SEOTools\Facades\OpenGraph::class,
-        'Twitter'   => Artesaos\SEOTools\Facades\TwitterCard::class,
-        // or
-        'SEO' => Artesaos\SEOTools\Facades\SEOTools::class,
+//        'SEOMeta'   => Artesaos\SEOTools\Facades\SEOMeta::class,
+//        'OpenGraph' => Artesaos\SEOTools\Facades\OpenGraph::class,
+//        'Twitter'   => Artesaos\SEOTools\Facades\TwitterCard::class,
+//        // or
+//        'SEO' => Artesaos\SEOTools\Facades\SEOTools::class,
 
     ],
 
